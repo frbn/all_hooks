8ea1268f43485377d23aa53901086e55f4eff119
Franck Boudehen <frbn@free.fr>
2025-05-02 14:33:01 +0200

    Update all_hooks--0.1.sql

--- all_hooks--0.1.sql
+++ all_hooks--0.1.sql
@@ -1,5 +1,5 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION all_hooks; LOAD 'all_hooks';" to load this file. \quit
+\echo Use "CREATE EXTENSION all_hooks or LOAD 'all_hooks';" to load this file. \quit
 
 load 'all_hooks';
 
